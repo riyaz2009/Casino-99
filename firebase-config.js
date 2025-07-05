8cbb1ff9ebb1e6af519e64ae6f85cd9eee1f6232
riyaz2009 <rafsanhridoy8236@gmail.com>
2025-07-05 16:28:54 +0600

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBHNMw7Vp7ag2TJfmaRfAOV63ckJeRs4qU",
-  authDomain: "casino-99-8db99.firebaseapp.com",
-  projectId: "casino-99-8db99",
-  storageBucket: "casino-99-8db99.firebasestorage.app",
-  messagingSenderId: "150835160784",
-  appId: "1:150835160784:web:e9f427154531eb2ad060d5",
-  measurementId: "G-N7SYVSSN8Y"
-};
